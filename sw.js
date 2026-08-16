72af2c6f397abd7c2321b9b6509e84e197fd5cef
Nexus MK <blowexposed@gmail.com>
2026-08-16 17:38:55 -0300

    Adiciona cancelamentos e controle operacional

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'motoja-conchal-v21-aviso-obrigatorio';
+const CACHE_NAME = 'motoja-conchal-v22-cancelamentos';
 const ARQUIVOS = ['./', './index.html', './motoboy.html', './dono.html', './motorista.webmanifest', './motorista-icon.svg'];
 
 self.addEventListener('install', (event) => {
