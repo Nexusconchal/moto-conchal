d1da88b39b91224681a6ad2c30db629e2372cc27
Nexus MK <blowexposed@gmail.com>
2026-08-16 17:29:05 -0300

    Envia Pix somente apos aceite do motoboy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'motoja-conchal-v18-trava-aceite';
+const CACHE_NAME = 'motoja-conchal-v20-pix-apos-aceite';
 const ARQUIVOS = ['./', './index.html', './motoboy.html', './dono.html', './motorista.webmanifest', './motorista-icon.svg'];
 
 self.addEventListener('install', (event) => {
