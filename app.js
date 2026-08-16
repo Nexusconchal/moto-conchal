5bfccffa77f88e30971e447612355d3ee9010a43
Nexus MK <blowexposed@gmail.com>
2026-08-15 21:08:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// app.js
 const NUMERO_WHATSAPP_EMPRESA = "5519992306488";
 
-const MENSAGEM_PADRAO = 
-  "Olá! Agradecemos seu contato! \uD83C\uDFCD\uFE0F\n" +
+const MENSAGEM_PADRAO =
+  "Olá! Agradecemos seu contato! 🏍️\n" +
   "Como podemos te ajudar hoje?\n\n" +
   "Para solicitar uma corrida, preencha:\n" +
-  "\uD83D\uDC64 *Nome:* \n" +
-  "\uD83D\uDCCD *Endereço de onde você está:* \n" +
-  "\uD83C\uDFAF *Endereço de destino:* \n\n" +
+  "👤 *Nome:* \n" +
+  "📍 *Endereço de onde você está:* \n" +
+  "🎯 *Endereço de destino:* \n\n" +
   "Qualquer problema, entre em contato por aqui mesmo que o suporte vai te ajudar!";
 
 document.getElementById('btn-pedir').addEventListener('click', () => {
