943a91858c2c27a65c90f7e5f0394715dca9d767
Nexus MK <blowexposed@gmail.com>
2026-08-15 21:09:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const NUMERO_WHATSAPP_EMPRESA = "5519992306488";
 
 const MENSAGEM_PADRAO =
-  "Olá! Agradecemos seu contato! 🏍️\n" +
+  "Olá! Agradecemos seu contato! \u{1F3CD}\uFE0F\n" +
   "Como podemos te ajudar hoje?\n\n" +
   "Para solicitar uma corrida, preencha:\n" +
-  "👤 *Nome:* \n" +
-  "📍 *Endereço de onde você está:* \n" +
-  "🎯 *Endereço de destino:* \n\n" +
+  "\u{1F464} *Nome:* \n" +
+  "\u{1F4CD} *Endereço de onde você está:* \n" +
+  "\u{1F3AF} *Endereço de destino:* \n\n" +
   "Qualquer problema, entre em contato por aqui mesmo que o suporte vai te ajudar!";
 
-document.getElementById('btn-pedir').addEventListener('click', () => {
+document.getElementById("btn-pedir").addEventListener("click", () => {
   const texto = encodeURIComponent(MENSAGEM_PADRAO);
   const link = `https://wa.me/${NUMERO_WHATSAPP_EMPRESA}?text=${texto}`;
-  window.open(link, '_blank');
+
+  window.open(link, "_blank");
 });
