523fa481b1f9017751bc6a47879b7a45c41c8ccc
Nexus MK <blowexposed@gmail.com>
2026-08-16 12:34:23 -0300

    Impede aceite duplicado de corrida

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'motoja-conchal-v17-nome-novo';
+const CACHE_NAME = 'motoja-conchal-v18-trava-aceite';
 const ARQUIVOS = ['./', './index.html', './motoboy.html', './dono.html', './motorista.webmanifest', './motorista-icon.svg'];
 
 self.addEventListener('install', (event) => {
