2f2a5133ef1bde0b16bc03a590b673a94c8e6667
Nexus MK <blowexposed@gmail.com>
2026-08-19 00:19:03 -0300

    Refina logo Nexus MotoJa

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nexus-motoja-v31-push';
+const CACHE_NAME = 'nexus-motoja-v32-logo';
 const ARQUIVOS = ['./', './index.html', './motoboy.html', './dono.html', './cliente.webmanifest', './motorista.webmanifest', './dono.webmanifest', './firebase-messaging-sw.js', './motorista-icon.svg', './nexus-motoja-icon-180.png', './nexus-motoja-icon-192.png', './nexus-motoja-icon-512.png'];
 
 self.addEventListener('install', (event) => {
