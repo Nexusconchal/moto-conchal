ce0c459360d0de4cef8167c4c36d1a8cdfac1712
Nexus MK <blowexposed@gmail.com>
2026-08-22 12:37:15 -0300

    Agrupa opcoes de lanche pizza e pastel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nexus-motoja-v43-recebedor-obrigatorio';
+const CACHE_NAME = 'nexus-motoja-v44-comida-agrupada';
 const ARQUIVOS = ['./', './index.html', './motoboy.html', './dono.html', './empresa.html', './cliente.webmanifest', './motorista.webmanifest', './dono.webmanifest', './firebase-messaging-sw.js', './nexus-motoja-logo-mark.png', './motorista-icon.svg', './nexus-motoja-icon-180.png', './nexus-motoja-icon-192.png', './nexus-motoja-icon-512.png'];
 
 self.addEventListener('install', (event) => {
