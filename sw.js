927ecec50169957f6564c526552bf60a45df5ac1
Nexus MK <blowexposed@gmail.com>
2026-08-22 12:28:28 -0300

    Ajusta app de empresas para delivery e encomendas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nexus-motoja-v38-entregas';
+const CACHE_NAME = 'nexus-motoja-v39-delivery-encomendas';
 const ARQUIVOS = ['./', './index.html', './motoboy.html', './dono.html', './empresa.html', './cliente.webmanifest', './motorista.webmanifest', './dono.webmanifest', './firebase-messaging-sw.js', './nexus-motoja-logo-mark.png', './motorista-icon.svg', './nexus-motoja-icon-180.png', './nexus-motoja-icon-192.png', './nexus-motoja-icon-512.png'];
 
 self.addEventListener('install', (event) => {
